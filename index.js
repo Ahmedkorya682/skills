b9933cd0c9555c52cf3716ccce85ccf4037357df
Ahmedkorya682 <111289577+Ahmedkorya682@users.noreply.github.com>
2022-08-18 15:16:55 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,186 @@
+/*let x=[1,2,3,4,5,6]
+
+
+let y=x.map((i)=>{
+
+return i
+
+
+})
+console.log(y);
+
+let z=x.filter((i)=>{
+
+    return i>3
+    
+    
+    })
+    console.log(z);
+
+
+    let e=x.find((i)=>{
+
+        return i>3
+        
+        
+        })
+        console.log(e);
+
+        let korya=(x=[])=>{
+
+       let y=   x.filter((i)=>{
+return i%2==0
+          })
+          return y
+        }
+
+        let r=korya(x)
+        console.log(r)*/
+
+      /*  function test(){
+return new Promise((resolve,reject)=>{
+
+  setTimeout(()=>{
+
+    console.log("test");
+    resolve()
+  },5000)
+})
+
+
+
+        }
+
+        
+        function test2(){
+          return new Promise((resolve,reject)=>{
+          
+            setTimeout(()=>{
+          
+              console.log("test2");
+              resolve()
+            },3000)
+          })
+          
+          
+          
+                  }
+
+                  function test3(){
+
+
+                    setTimeout(()=>{
+                      console.log("test3")
+                    },1000)
+                  }
+
+
+                  async function korya(){
+
+await test()
+await test2()
+test3()
+
+
+                  }
+
+                  korya()*/
+
+
+
+            /*      let Arr=[9,1,5]
+
+              let  x=   Arr.filter((i)=>{
+
+                    return i>3
+                  
+
+                  })
+                  console.log(x);
+
+
+
+
+                 
+
+            xu=Arr.forEach((i)=>{
+
+              
+
+                  return i+1
+                })
+
+
+                console.log(xu)
+                
+               
+                var arrr=['ahmed','omar','tota']*/
+
+          /*      function korya1(){
+
+      return new Promise((resolve,reject)=>{
+
+setTimeout(()=>{
+  console.log('korya1');
+  resolve()
+},5000)
+
+
+
+
+
+      })
+
+
+
+
+                }
+
+
+                function korya2(){
+                  return new Promise((resolve,reject)=>{
+         setTimeout(()=>{
+
+          console.log("korya2")
+          resolve()
+         },3000)
+
+        
+
+
+                  })
+
+
+
+                }
+
+               /* function korya3(){
+                  return new Promise((resolve,reject)=>{
+         setTimeout(()=>{
+
+          console.log("korya3")
+          resolve()
+         },1000)
+
+       
+
+
+
+                  })
+
+
+
+                }
+
+              async  function test(){
+
+
+                await korya1()
+          await korya2()
+                await korya3()
+
+
+                }
+
+
+                test()*/
